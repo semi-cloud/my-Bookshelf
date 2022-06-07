4ed22f2f66ab8ad156b96a6b6c811a3818402fd0
semi-cloud <sksmsrkdtpal@naver.com>
2022-06-07 22:31:31 +0900

    :building_construction: 앱 폴더 구조 변경

--- book_pages/apps.py
+++ book_pages/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class NeighborBookConfig(AppConfig):
+class BookPagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'neighbor_book'
+    name = 'book_pages'
