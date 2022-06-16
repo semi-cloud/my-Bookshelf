71d0f2ca9cd8547233144106fec78f73344ddf27
semi-cloud <sksmsrkdtpal@naver.com>
2022-06-16 17:03:00 +0900

    :sparkles: 로그아웃 기능 추가 (#4)

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
   path('', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-  path('signup/', views.signup, name='signup')
+  path('signup/', views.signup, name='signup'),
+  path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- main/settings.py
+++ main/settings.py
@@ -124,6 +124,7 @@ USE_TZ = False
 
 # Auth
 LOGIN_REDIRECT_URL = '/book'
+LOGOUT_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
